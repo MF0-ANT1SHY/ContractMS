e9be68c211289384eedb137856e00d71cde12c98
MF0-ANT1SHY <komeijiddz@gmail.com>
2022-06-02 11:49:50 +0800

    complete UserService layer

--- src/main/java/com/ccos/contract/service/UserService.java
+++ src/main/java/com/ccos/contract/service/UserService.java
@@ -1,8 +1,65 @@
 package com.ccos.contract.service;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
 import com.ccos.contract.dao.UserDao;
 import com.ccos.contract.po.User;
+import com.ccos.contract.vo.ResultInfo;
 
+/*
+input: userName
+output: resultInfo
+判断参数是否null
+    if null
+        set ResultInfo's statecode&tips
+        return ResultInfo
+    else
+        getResultInfobyName()
+        if ResultInfo == null
+            set ResultInfo's statecode&tips
+            return ResultInfo
+        else
+            if ResultInfo.pwd != form.pwd
+                set ResultInfo's statecode&tips
+                return ResultInfo
+            else
+                set ...
+                return ...
+ */
 public class UserService {
     private UserDao userDao = new UserDao();
+    public ResultInfo<User> userLogin(String userName, String userPwd) {
+        ResultInfo<User> resultInfo = new ResultInfo<>();
+        //1. judge arg == null
+        if (StrUtil.isBlank(userName)||StrUtil.isBlank(userPwd)){
+            //set state code == 0
+            resultInfo.setCode(0);
+            resultInfo.setMsg("username&password can't be null!");
+            //return resultInfo
+            return resultInfo;
+        }
+        //2. search by username
+        User user = userDao.queryUserByName(userName);
+
+        //3. judge user == null
+        if (user==null){
+            resultInfo.setCode(0);
+            resultInfo.setMsg("this user doesn't exit!");
+            //return resultInfo
+            return resultInfo;
+        }
+
+        //4. check password
+        // encrypto pwd from frontend by MD5
+        userPwd = DigestUtil.md5Hex(userPwd);
+        if (!userPwd.equals(user.getUpwd())){
+            resultInfo.setCode(0);
+            resultInfo.setMsg("password isn't correct!");
+            return resultInfo;
+        }
+
+        resultInfo.setCode(1);
+        resultInfo.setResult(user);
+        return resultInfo;
+    }
 }
